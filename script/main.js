b3be22660fe0ec62410e9e24096643643b5ff619
Enrique Tasa <tasa@enriquetasa.com>
2019-08-19 19:07:51 +0100

    August 2019 update and minor UI changes

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-var copyrightPhrase = "made with love in Valencia, Spain by Enrique Tasa. April 2019.";
+var copyrightPhrase = "made with love in Valencia, Spain and Manchester, UK by Enrique Tasa. August 2019.";
 
 function startUp(){
     putCopyrightInfo();
