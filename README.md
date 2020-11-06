15bbc37ca69965ecb621c704a7b3196ef02e17a5
Enrique Tasa <tasa@enriquetasa.com>
2020-11-06 20:59:33 +0000

    Added show me something and made small changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # enriquetasa.com
 
-This is my personal website. It's a little hub for me to have an online CV and to showcase the stuff I've done and made over the past few years. It's deliberately extremely simple, both because I believe that it serves the purpose it was created to serve better that way. 
+This is my personal website. It's a little hub for me to have an online CV and to showcase the stuff I've done and made over the past few years. It's deliberately extremely simple, because I believe that it serves the purpose it was created to serve better that way. 
--- public/script/main.js
+++ public/script/main.js
@@ -1,4 +1,4 @@
-var copyrightPhrase = "made with love in Valencia, Spain and Manchester, UK by Enrique Tasa. June 2020.";
+var copyrightPhrase = "made with love in Valencia, Spain and Manchester, UK by Enrique Tasa. March 2019 - November 2020.";
 
 function startUp(){
     putCopyrightInfo();
@@ -7,3 +7,27 @@ function startUp(){
 function putCopyrightInfo(){
     document.getElementById('copyrightInfo').innerHTML = copyrightPhrase;
 }
+
+var links = [
+    "youtu.be/vMnWK6apliQ", // six months in merchant marine
+    "youtu.be/OPNZsKgzHGw", // there can only be one good boy in this town
+    "youtu.be/46ZXl-V4qwY", // ancient rome in 20 minutes
+    "youtu.be/Khfe3jBuq8c", // watch this if you want to smile (hank green)
+    "youtu.be/KzPdfDDkCik", // take a closer look at that snout
+    "youtu.be/nTQWtJVlJhk", // chase that happy
+    "youtu.be/EAqGGpj0Ans", // classic sesame street
+    "youtu.be/MCwGOMloBPk", // documentary
+    "poetryfoundation.org/poems/51642/invictus", // invictus
+    "poetryfoundation.org/poems/56159/this-is-just-to-say", // this is just to say
+    "poetryfoundation.org/poems/42889/hope-is-the-thing-with-feathers-314", // hope is the thing with feathers
+    "biblegateway.com/passage/?search=1+Peter+4%3A8-4%3A11&version=NIV]", // 1 Peter 4:8-11
+    "biblegateway.com/passage/?search=Matthew%2022:37-39&version=NIV", // Matthew 22:37-39
+    "artic.edu/artworks/111628/nighthawks" // nighthawks
+];
+
+function showMeSomething(){
+    var randIdx = Math.random() * links.length;
+    randIdx = parseInt(randIdx, 10);
+    var link = 'http://' + links[randIdx];
+    window.open(link);
+};
